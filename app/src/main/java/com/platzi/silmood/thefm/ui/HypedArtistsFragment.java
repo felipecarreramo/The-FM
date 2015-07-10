903ff3a98f321f7f812f5f757c90279aa9e88eba
pipecamo <pipecamo@gmail.com>
2015-07-10 00:09:32 -0500

    recyclerview -cardview con dummy data, para empezar a usar retrofit

--- app/src/main/java/com/platzi/silmood/thefm/ui/HypedArtistsFragment.java
+++ app/src/main/java/com/platzi/silmood/thefm/ui/HypedArtistsFragment.java
@@ -13,6 +13,10 @@ import android.view.ViewGroup;
 
 import com.platzi.silmood.thefm.MainActivity;
 import com.platzi.silmood.thefm.R;
+import com.platzi.silmood.thefm.domain.Artist;
+import com.platzi.silmood.thefm.ui.adapter.HypedArtistsAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -37,19 +41,39 @@ public class HypedArtistsFragment extends Fragment{
     public static final String LOG_TAG = HypedArtistsFragment.class.getName();
 
     private RecyclerView mHypedArtistsList;
+    private HypedArtistsAdapter adapter;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.adapter = new HypedArtistsAdapter(getActivity());
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_hyped_artists, container, false);
-
         mHypedArtistsList = (RecyclerView) root.findViewById(R.id.hyped_artists_list);
+
+        setupArtitsList();
+        setDummyContent();
 
         return root;
     }
 
     private void setupArtitsList(){
         mHypedArtistsList.setLayoutManager(new GridLayoutManager(getActivity(), NUM_COLUMNS));
+        mHypedArtistsList.setAdapter(this.adapter);
+        mHypedArtistsList.addItemDecoration(new ItemOffsetDecoration(getActivity(),R.integer.offset));
+    }
+
+    private void setDummyContent(){
+
+        ArrayList<Artist> artists = new ArrayList<>();
+        for (int i = 0 ; i < 10; i++){
+            artists.add(new Artist("Artist "+i));
+        }
+
+        this.adapter.addAll(artists);
     }
 }
---
+++ app/src/main/java/com/platzi/silmood/thefm/ui/adapter/HypedArtistsAdapter.java
@@ -0,0 +1,85 @@
+package com.platzi.silmood.thefm.ui.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.platzi.silmood.thefm.R;
+import com.platzi.silmood.thefm.domain.Artist;
+
+import java.util.ArrayList;
+
+/**
+ * Created by FelipeCarrera on 9/07/15.
+ */
+public class HypedArtistsAdapter  extends RecyclerView.Adapter<HypedArtistsAdapter.HypedArtistViewHolder> {
+
+    private Context context;
+    private ArrayList<Artist> artists;
+
+
+    public HypedArtistsAdapter( Context context) {
+        this.context = context;
+        this. artists = new ArrayList<>();
+    }
+
+
+    @Override
+    public HypedArtistViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(this.context).
+                inflate(R.layout.item_hyped_artists, parent, false);
+
+
+        return new HypedArtistViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(HypedArtistViewHolder holder, int position) {
+
+        Artist art = this.artists.get(position);
+        holder.setArtistName(art.getName());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.artists.size();
+    }
+
+    public void addAll(@NonNull ArrayList<Artist> artists){
+
+        if (artists == null)
+            throw new NullPointerException("The items cannot be null");
+
+        this.artists.addAll(artists);
+        notifyItemRangeInserted(getItemCount() - 1, artists.size());
+        //notifyDataSetChanged();
+
+    }
+
+    public class HypedArtistViewHolder extends RecyclerView.ViewHolder{
+
+        TextView artistName;
+        ImageView artistImage;
+
+        public HypedArtistViewHolder(View itemView) {
+            super(itemView);
+
+            this.artistName = (TextView) itemView.findViewById(R.id.txt_name);
+            this.artistImage = (ImageView) itemView.findViewById(R.id.img_artist);
+
+        }
+
+        public void setArtistName(String name){
+            this.artistName.setText(name);
+        }
+    }
+
+}
